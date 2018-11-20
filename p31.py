bae0121377ad58d4770b1aab7bc5b0c29698ff66
joshua7linares <42164091+joshua7linares@users.noreply.github.com>
2018-11-20 23:11:42 +0000

    Add files via upload

---
+++ p31.py
@@ -0,0 +1,35 @@
+#recibe una matriz y la muestra
+
+
+print "Linares Alonso Joshua A."
+matriz1=[]
+res=0
+
+def crear(matriz):
+  for i in range (2):
+     matriz.append([0]*2)
+
+def ingresar(matriz):
+  for i in range(2):
+     for j in range(2):
+       print "fila: ", i+1, "coluumna: ", j+1
+       matriz[i][j]=input("ingresa valor: ")
+
+def mostrar(matriz):  
+ for i in matriz:
+    for j in i:
+      print j,
+    print
+
+def resul(matriz1,res):
+      res=matriz1[0][0]*matriz1[1][1]-matriz1[0][1]*matriz1[1][0]
+      if res==0:
+        print "esta matriz no tiene inversa"
+      else:
+          print "esta matriz si tiene inversa"  
+
+
+crear(matriz1)
+ingresar(matriz1)
+mostrar(matriz1)
+resul(matriz1,res)
